884826e127921d90ff33b7082c33d07021213509
uzer2281 <nastyamoshko@gmail.com>
2023-11-30 12:57:02 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const PORT = 5002;
+const app = express();
+app.use(express.json());
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.status(200).json('Сервер работает');
+});
+
+app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT));
